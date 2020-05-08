c662c2f4f199b4fb1449014bae2570820bbd36bc
Great-Emeka <greatestgreatemeka@gmail.com>
2020-05-08 17:41:33 +0100

    fixed some Js burgs

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
 //All my selectors goes here
-
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
 
 //Event Listeners
+todoButton.addEventListener("click", addTodo);
 
 
 //Functions
+function addTodo(event) {
+    //to prevent the form from submitting we do
+    event.preventDefault();
+    
+    //Todo Div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    //create Li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    //Check mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fa fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+
+    //Check trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fa fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+    //Appending to List
+    todoList.appendChild(todoDiv);
+}
